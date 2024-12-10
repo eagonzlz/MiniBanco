60bf26d4766e6cdb0291ad09012452fb56029e35
eagonzlz <101129923+eagonzlz@users.noreply.github.com>
2024-12-10 12:28:13 -0400

    Initial Commit

---
+++ MiniBanco/MiniBancoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBancoApp.swift
+//  MiniBanco
+//
+//  Created by ArcaneHollow on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniBancoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
